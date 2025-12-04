24653f56f358229abb0adcf00179e2d7dd25fc10
sohrab <sohrab@sohrabhossain.com>
2025-12-04 12:25:47 +0000

    format code using terraform fmt.

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-remotestate-dockyard43526"
-    key    = "s3_static_web/terraform.tfstate"
-    region = "us-east-1"
+    bucket       = "terraform-remotestate-dockyard43526"
+    key          = "s3_static_web/terraform.tfstate"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
