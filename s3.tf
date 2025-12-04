224f0d0d28c25ec6e3365e40a7c8002515024308
sohrab <sohrab@sohrabhossain.com>
2025-12-04 11:21:01 +0000

    Fix terraform formatting

--- s3.tf
+++ s3.tf
@@ -1,76 +1,76 @@
 resource "aws_s3_bucket" "static_website_bucket" {
-    bucket_prefix = var.bucket_prefix
-    region = var.bucket_region
-    tags = var.tags
+  bucket_prefix = var.bucket_prefix
+  region        = var.bucket_region
+  tags          = var.tags
 }
 
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
-    bucket = aws_s3_bucket.static_website_bucket.id
-    versioning_configuration {
-      status = var.aws_s3_bucket_versioning
-    }
+  bucket = aws_s3_bucket.static_website_bucket.id
+  versioning_configuration {
+    status = var.aws_s3_bucket_versioning
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
-    bucket = aws_s3_bucket.static_website_bucket.id
+  bucket = aws_s3_bucket.static_website_bucket.id
 
-    index_document {
-      suffix = "index.html"
-    }
+  index_document {
+    suffix = "index.html"
+  }
 
-    error_document {
-      key = "error.html"
-    }
+  error_document {
+    key = "error.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-    bucket = aws_s3_bucket.static_website_bucket.id
+  bucket = aws_s3_bucket.static_website_bucket.id
 
-    block_public_acls       = false
-    block_public_policy     = false
-    ignore_public_acls      = false
-    restrict_public_buckets = false
-  
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-    bucket = aws_s3_bucket.static_website_bucket.id
-    depends_on = [ aws_s3_bucket_public_access_block.public_access ]
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Sid       = "PublicReadGetObject"
-          Effect    = "Allow"
-          Principal = "*"
-          Action    = "s3:GetObject"
-          Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
-        }
-      ]
-    })
+  bucket     = aws_s3_bucket.static_website_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_object" "website_files" {
-    for_each = fileset("${path.module}/www", "**")
+  for_each = fileset("${path.module}/www", "**")
 
-    bucket = aws_s3_bucket.static_website_bucket.id
-    key    = each.value
-    source = "${path.module}/www/${each.value}"
-    etag   = filemd5("${path.module}/www/${each.value}")
-    content_type = lookup({
-      html = "text/html"
-      css  = "text/css"
-      js   = "application/javascript"
-      png  = "image/png"
-      jpg  = "image/jpeg"
-      gif  = "image/gif"
-    }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
-  
+  bucket = aws_s3_bucket.static_website_bucket.id
+  key    = each.value
+  source = "${path.module}/www/${each.value}"
+  etag   = filemd5("${path.module}/www/${each.value}")
+  content_type = lookup({
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    gif  = "image/gif"
+  }, split(".", each.value)[length(split(".", each.value)) - 1], "application/octet-stream")
+
 }
 
 output "website_endpoint" {
-    description = "The endpoint URL of the S3 static website"
-    value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
+  description = "The endpoint URL of the S3 static website"
+  value       = aws_s3_bucket_website_configuration.website_config.website_endpoint
 }
 
--- variable.tf
+++ variable.tf
@@ -3,13 +3,13 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
   description = "The AWS CLI profile to use for authentication"
   type        = string
-  default = "terraform_dockyard"
+  default     = "terraform_dockyard"
 }
 
 # s3 bucket configuration variables
@@ -17,29 +17,29 @@ variable "aws_s3_bucket_versioning" {
   description = "Enable versioning for the S3 bucket"
   type        = string
   default     = "Enabled"
-  
+
 }
 
 variable "tags" {
   description = "A map of tags to assign to the resources"
   type        = map(string)
-  default     = {
+  default = {
     Environment = "Dev"
     Project     = "S3StaticWebsite"
   }
-  
+
 }
 
 variable "bucket_prefix" {
   description = "Prefix for the S3 bucket name"
   type        = string
   default     = "my-static-website-"
-  
+
 }
 
 variable "bucket_region" {
   description = "The AWS region where the S3 bucket will be created"
   type        = string
   default     = "us-east-1"
-  
+
 }
