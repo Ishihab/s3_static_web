977ab13b1db1566152be5b8edab575633dac7093
sohrab <sohrab@sohrabhossain.com>
2025-12-06 16:15:23 +0000

    remove public read access from S3 bucket policy

--- s3.tf
+++ s3.tf
@@ -24,16 +24,6 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-
-}
-
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket     = aws_s3_bucket.static_website_bucket.id
   depends_on = [aws_s3_bucket_public_access_block.public_access]
